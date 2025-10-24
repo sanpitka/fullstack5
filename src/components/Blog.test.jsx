cfb556e15a5d0b9b30a71c312ab1b400233d9aba
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-10-24 22:48:55 +0300

    5.14

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 import { expect } from 'vitest'
 
 test('renders title and author but not url or likes by default', () => {
@@ -19,4 +20,26 @@ test('renders title and author but not url or likes by default', () => {
   expect(screen.getByText(/Erkki Esimerkki/)).toBeDefined()
   expect(screen.queryByText('http://esimerkki.vuodatus.net')).toBeNull()
   expect(screen.queryByText('5 likes')).toBeNull()
+})
+
+test('shows url and likes when the view button is clicked', async () => {
+  const blog = {
+    title: 'Komponentin renderöinnin alkeet',
+    author: 'Ajokortti Körkort',
+    url: 'http://esimerkki.vuodatus.net',
+    likes: 5,
+    user: {
+        username: 'erkki',
+        name: 'Erkki Esimerkki'
+    }
+}
+  render(<Blog blog={blog} updateBlog={() => {}} removeBlog={() => {}}/>)
+
+  expect(screen.getByText(/Komponentin renderöinnin alkeet/)).toBeDefined()
+  expect(screen.getByText(/Ajokortti Körkort/)).toBeDefined()
+  const button = screen.getByText('view')
+  await userEvent.click(button)
+  expect(screen.getByText('http://esimerkki.vuodatus.net')).toBeDefined()
+  expect(screen.getByText('5 likes')).toBeDefined()
+  expect(screen.getByText('Erkki Esimerkki')).toBeDefined()
 })
