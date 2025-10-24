02774daed15c9a854c6a981c4e413c244bccd5c3
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-10-24 12:20:14 +0300

    5.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null) 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,11 +19,112 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+      </div>
+    )
+  }
+
+  const blogForm = () => {
+    return (
+      <div>
+        <h2>add new blog</h2>
+        <form /* onSubmit = {addBlog} */>
+          <div>
+            <label>
+              title:
+              <input
+                type="text"
+                value={title}
+                name="Title"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+            </label>
+          </div>
+          <div>
+            <label>
+            author:
+            <input
+              type="text"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+            </label>
+          </div>
+          <div>
+            <label>
+              url:
+              <input
+                type="text"
+                value={url}
+                name="Url"
+                onChange={({ target }) => setUrl(target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          {blogForm()}
+          <div style={{marginTop: '20px'}}>
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            )}
+          </div>
+        </div>
       )}
     </div>
   )
