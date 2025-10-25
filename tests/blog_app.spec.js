b53ca36f879586d112c4b9f4d8c66409ad36ecc0
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-10-25 15:00:16 +0300

    5.19

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,4 +1,4 @@
-const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { test, expect, beforeEach, afterEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -10,7 +10,6 @@ describe('Blog app', () => {
         password: 'heimuumit'
       }
     })
-
     await page.goto('http://localhost:5173')
   })
 
@@ -30,6 +29,8 @@ describe('Blog app', () => {
       await page.locator('input').first().fill('tofslan')
       await page.locator('input[type="password"]').fill('heimuumit')
       await page.getByRole('button', { name: 'login' }).click()
+			
+			await page.waitForLoadState('networkidle')
       await expect(page.getByText('Tove Jansson logged in')).toBeVisible()
     })
 
@@ -40,5 +41,24 @@ describe('Blog app', () => {
       await page.getByRole('button', { name: 'login' }).click()
       await expect(page.getByText('Wrong username or password')).toBeVisible()
     })
-  })
+    describe('When logged in', () => {
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'login' }).click()
+        await page.locator('input').first().fill('tofslan')
+        await page.locator('input[type="password"]').fill('heimuumit')
+        await page.getByRole('button', { name: 'login' }).click()
+        await expect(page.getByText('Tove Jansson logged in')).toBeVisible()
+      })
+
+			test('a new blog can be created', async ({ page }) => {
+				await page.getByRole('button', { name: 'add new blog' }).click()
+				await page.locator('input').first().fill('Taikatalvi')
+				await page.locator('input').nth(1).fill('Tove Jansson')
+				await page.locator('input').nth(2).fill('https://taikatalvi.blogspot.com')
+				await page.getByRole('button', { name: 'create' }).click()
+				await expect(page.getByText('Taikatalvi Tove Jansson')).toBeVisible()
+		
+			})
+		})
+	})
 })
