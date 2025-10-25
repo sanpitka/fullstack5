c1a99b675ec6da0dbf36f1db3a3f2d1b2c01a45e
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-10-25 11:31:41 +0300

    5.17

---
+++ tests/blog_app.spec.js
@@ -0,0 +1,27 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Tove Jansson',
+        username: 'tofslan',
+        password: 'heimuumit'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+    
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+    
+    await expect(page.locator('input').first()).toBeVisible()
+    await expect(page.locator('input[type="password"]')).toBeVisible()
+  })
+})
